acaf9816e47ccf510490cb80c2d5219c54cff775
kimys-kor <ys2life1@naver.com>
2021-12-18 17:30:57 +0900

    finish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    @Get()
+    home(){
+        return "Welcome to my Movie API";
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
